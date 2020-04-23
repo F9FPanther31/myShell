6f1cda8c3459232f7436092440f3bc258ad1690a
Reznov Yang <48404988+F9FPanther31@users.noreply.github.com>
2020-04-24 02:51:21 +0800

    Update load_config.cpp

--- src/load_config.cpp
+++ src/load_config.cpp
@@ -3,8 +3,9 @@
 int load_config(config *config){
     //get work dir
     string path =getcwd(NULL,0);
+    //set shell env dir
     path+="myshell";
-    setenv("myshell",path.c_str(),0);
+    setenv("shell",path.c_str(),1);
     //get hostname
     gethostname(config->host_name,sizeof(config->host_name));
     //get user_info
