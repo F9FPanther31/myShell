9f671ea72d0d59ad88aba7e377f2c704370f80c5
Reznov Yang <48404988+F9FPanther31@users.noreply.github.com>
2020-04-24 14:22:49 +0800

    Update load_config.cpp

--- src/load_config.cpp
+++ src/load_config.cpp
@@ -2,9 +2,9 @@
 
 int load_config(config *config){
     //get work dir
-    string path =getcwd(NULL,0);
+    string cwd =getcwd(NULL,0);
     //set shell env dir
-    path+="myshell";
+    cwd+="/myshell";
     setenv("shell",path.c_str(),1);
     //get hostname
     gethostname(config->host_name,sizeof(config->host_name));
